45e2a0e4b2c0f482d554432e7d75ae86ba883d59
Darren <darren.guthrie@uky.edu>
2024-10-18 19:25:52 -0700

    initial content

---
+++ data.js
@@ -0,0 +1,41 @@
+const a = {
+    location: {
+      feedBack: "#locateFeedback",
+      info: {},
+      turf: [],
+    },
+    map: {
+      div: "map",
+      options: {
+        zoomSnap: 1,
+        zoomControl: false,
+        zoom: 4,
+        center: [39.8283, -98.5795],
+      },
+      fitOptions: {
+        padding: [20, 20],
+        animate: false,
+      },
+      zoomOptions: {
+        position: "bottomright",
+      },
+    },
+    tiles: {
+      url: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+      options: {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      },
+    },
+    buttons: {
+      locate: {
+        id: "#geolocate-ui",
+      },
+    },
+    fences: [],
+    geojson: {},
+    page: {
+      header: "header",
+      footer: "footer",
+    },
+  };
