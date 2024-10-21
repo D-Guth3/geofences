a56029cec3a96a1c1b63f2375cd53b565559927f
Darren <darren.guthrie@uky.edu>
2024-10-21 08:43:37 -0700

    Working code
    push working code

--- data.js
+++ data.js
@@ -1,41 +1,41 @@
-const a = {
-    location: {
-      feedBack: "#locateFeedback",
-      info: {},
-      turf: [],
-    },
-    map: {
-      div: "map",
-      options: {
-        zoomSnap: 1,
-        zoomControl: false,
-        zoom: 4,
-        center: [39.8283, -98.5795],
+  const a = {
+      location: {
+        feedBack: "#locateFeedback",
+        info: {},
+        turf: [],
       },
-      fitOptions: {
-        padding: [20, 20],
-        animate: false,
+      map: {
+        div: "map",
+        options: {
+          zoomSnap: 1,
+          zoomControl: false,
+          zoom: 4,
+          center: [39.8283, -98.5795],
+        },
+        fitOptions: {
+          padding: [20, 20],
+          animate: false,
+        },
+        zoomOptions: {
+          position: "bottomright",
+        },
       },
-      zoomOptions: {
-        position: "bottomright",
+      tiles: {
+        url: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+        options: {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        },
       },
-    },
-    tiles: {
-      url: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
-      options: {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      buttons: {
+        locate: {
+          id: "#geolocate-ui",
+        },
       },
-    },
-    buttons: {
-      locate: {
-        id: "#geolocate-ui",
+      fences: [],
+      geojson: {},
+      page: {
+        header: "header",
+        footer: "footer",
       },
-    },
-    fences: [],
-    geojson: {},
-    page: {
-      header: "header",
-      footer: "footer",
-    },
-  };
+    };
